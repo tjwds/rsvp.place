8ac1a17c6bed1dd99a81350024871d963e905941
Joe Woods <code@joewoods.dev>
2024-07-11 14:50:08 -0400

    Map: change tile provider

--- src/elements/DynamicMap.js
+++ src/elements/DynamicMap.js
@@ -10,10 +10,8 @@ const Map = ({ children, className, width, height, ...rest }) => {
   return (
     <MapContainer className={newStyles} zoom={12} {...rest}>
       <TileLayer
-        // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        // attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       <Marker
         position={rest.center}
